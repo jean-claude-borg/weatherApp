9a0170f25250d46cbf3ad0620ed7d0e4a93be508
jean-claude-borg <jeanclaudeborg2001@gmail.com>
2023-09-15 02:58:27 +0200

    update

--- App.js
+++ App.js
@@ -10,47 +10,49 @@ import { MD3LightTheme,
          Text,
          IconButton, 
          Menu,
+         Searchbar,
+         Appbar,
+         Divider
         } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import * as Location from 'expo-location';
+import { BlurView } from "expo-blur";
 import apiResponseRealtimeJSON from './apiResponseJson.json'
 import apiResponseForecastJSON from './apiResponseForecastJson.json'
 
+const Stack = createStackNavigator();
 
 const colorScheme = "dark";
+const theme = colorScheme == 'dark' ? { ...MD3DarkTheme} : { ...MD3LightTheme};
 
-const theme = 
-  colorScheme == 'dark'
-  ? { ...MD3DarkTheme}
-  : { ...MD3LightTheme};
-
-function HeaderSection({apiResponseRealtime, showMenu, setMenu}){
+function HeaderSection({navigation, apiResponseRealtime, showMenu, setMenu}){
   
   const cityName = apiResponseRealtime.location.name;
   const countryName = apiResponseRealtime.location.country;
-  const icon1 = require('./assets/icons/settingsIcon.png');
-
 
   return(
     <View style={styles.headerSection}>
+
       <View style={{backgroundColor:"transparent", flex:1}}>
         <Text variant='displaySmall' style={styles.cityName}>{cityName}</Text>
         <Text variant='titleMedium' style={styles.countryName}>{countryName}</Text>
       </View>
-      {/* <Button icon={icon1} 
-              contentStyle={{flexDirection:"row-reverse", width:40, height:40}}
-              style={{flex:0.02, width:40, height:40, flexDirection:"row-reverse"}}/> */}
+
       <Menu
         visible={showMenu}
         onDismiss={() => {setMenu(false)}}
         anchor={
-            <IconButton icon="settings-helper" 
-                      size={50} 
-                      style={{backgroundColor:"transparent", transform: [{ rotate: '-90deg' }], alignSelf:"flex-start"}}
-                      onPress={() => setMenu(true)}
+            <IconButton 
+                        icon={require('./assets/icons/settings-helper.png')}
+                        style={{transform: [{ rotate: '-90deg' }], justifyContent:"center", alignContent:"center"}}
+                        mode='default'
+                        size={25}
+                        onPress={() => setMenu(true)}
           />
         }>
-        <Menu.Item onPress={() => {}} title="Cities" />
-        <Menu.Item onPress={() => {}} title="Settings" />
+        <Menu.Item onPress={() => {navigation.navigate("Cities")}} title="Cities" />
+        <Menu.Item onPress={() => {navigation.navigate("Settings")}} title="Settings" />
       </Menu>
     </View>
   )
@@ -60,8 +62,9 @@ function WeatherIconSection({iconLink}){
   return(
     <View style={styles.weatherIconSection}>
       <Image
-        source={{ uri: `https:${iconLink}` }}
-        style={{width:"95%", height: "90%"}}
+        // source={{ uri: `https:${iconLink}` }}
+        source={require('./assets/icons/sunny2.png')}
+        style={{width:"100%", height: "100%"}}
       />
     </View>
   )
@@ -69,11 +72,6 @@ function WeatherIconSection({iconLink}){
 
 function WeatherOverviewSection({apiResponseRealtime}){
 
-  if (!apiResponseRealtime) {
-    return null;
-  }
-
-  const weatherCodeToday = apiResponseRealtime.current.condition.code;
   const weatherDescriptionToday = apiResponseRealtime.current.condition.text;
   const weatherIconToday = apiResponseRealtime.current.condition.icon;
   const temperatureToday = Math.floor(apiResponseRealtime.current.temp_c);
@@ -83,7 +81,7 @@ function WeatherOverviewSection({apiResponseRealtime}){
       <WeatherIconSection iconLink={weatherIconToday}/>
       <View style={{display:"flex", flexDirection:"column", backgroundColor:"transparent"}}>
         <Text variant="displayLarge" style={{alignSelf:"center"}}>{temperatureToday}{'\u00B0'}</Text>
-        <Text variant="titleMedium" style={styles.countryName}>{weatherDescriptionToday}</Text>
+        <Text variant="titleMedium" style={{alignSelf:"center"}}>{weatherDescriptionToday}</Text>
       </View>
     </View>
   )
@@ -198,21 +196,82 @@ function DailyForecastSection({apiResponseRealtime, apiResponseForecast}){
   )
 }
 
-// function SearchBar(){
-  //   return(
-  //     <Input
-//       placeholder='Search Location'
-//     />
-//   )
-// }
+function HomeScreen({navigation, apiResponseRealtime, apiResponseForecast, showMenu, setMenu}){
+  return(
+    <>
+      {/* background image */}
+      <Image 
+        source={require("./assets/backgrounds/background2.jpg")}
+        blurRadius={9}
+        style={styles.background}
+      />
+      <StatusBar style='auto' />
+      <PaperProvider theme={theme}>
+        <SafeAreaView style={{flex:1}}>
+          <View style={{flex: 1, justifyContent: 'top', alignItems: 'flex-start', backgroundColor:"transparent"}}>
+            <HeaderSection apiResponseRealtime={apiResponseRealtime} showMenu={showMenu} setMenu={setMenu} navigation={navigation}/>
+            <WeatherOverviewSection apiResponseRealtime={apiResponseRealtime}/>
+            <DailyForecastSection apiResponseRealtime={apiResponseRealtime} apiResponseForecast={apiResponseForecast}/>
+          </View>
+        </SafeAreaView>
+      </PaperProvider>
+    </>
+  )
+}
+
+function CitiesPageCity({cityName}){
+  return(
+    <Surface style={{display:"flex",width:"100%", height:100, justifyContent:"center"}}>
+      <View style={{marginLeft:"7%"}}>
+        <Text variant='headlineLarge'>{cityName}</Text>
+      </View>
+    </Surface>
+  )
+}
+
+function CitiesPage({navigation, citiesList, setCitiesList}){
+
+  return(
+    <>
+      <PaperProvider theme={theme}>
+        <Appbar.Header>
+          <Appbar.BackAction onPress={() => {navigation.navigate("HomeScreen")}} />
+          <Appbar.Content title="Your Saved Cities" />
+        </Appbar.Header>
+        <Searchbar
+          placeholder="Search For New City"
+          mode="view"
+        />
+        <ScrollView style={{marginTop:25}}>
+          {
+            citiesList.map((city, index) => (
+              <>
+                <CitiesPageCity key={index} cityName={city}/>
+                <Divider></Divider>
+              </>
+            ))
+          }
+        </ScrollView>
+      </PaperProvider>
+    </>
+  )
+}
+
+function SettingsPage(){
+  return(
+    <>
+    </>
+  )
+}
 
 export default function App() {
 
   const [showMenu, setMenu] = useState(false);
 
   const [locationCoords, setLocationCoords] = useState(null);
-  const [apiResponseRealtime, setApiResponseRealtime] = useState(null);
-  const [apiResponseForecast, setApiResponseForecast] = useState(null);
+  const [apiResponseRealtime, setApiResponseRealtime] = useState(apiResponseRealtimeJSON);
+  const [apiResponseForecast, setApiResponseForecast] = useState(apiResponseForecastJSON);
+  const [citiesList, setCitiesList] = useState([apiResponseForecast.location.name]);
 
   const apiKey = "6533b15ba3e54edabfb141617231309";
   const allowApiRealtime = false;
@@ -245,11 +304,11 @@ export default function App() {
       {
         const options = {method: 'GET', headers: {accept: 'application/json'}};
         try {
-          fetch(apiUrlRealtime, options)
+          await fetch(apiUrlRealtime, options)
           .then(response => response.json())
           .then(data => {
             setApiResponseRealtime(data);
-            console.log(JSON.stringify(data, null, 2));
+            setCitiesList([data.location.name]);
         })
           .catch(err => console.error(err));
         } catch (err) {
@@ -264,11 +323,11 @@ export default function App() {
       {
         const options = {method: 'GET', headers: {accept: 'application/json'}};
         try {
-          fetch(apiUrlForecast, options)
+          await fetch(apiUrlForecast, options)
           .then(response => response.json())
           .then(data => {
             setApiResponseForecast(data);
-            console.log(JSON.stringify(data, null, 2));
+            setCitiesList([data.location.name]);
         })
           .catch(err => console.error(err));
         } catch (err) {
@@ -278,35 +337,39 @@ export default function App() {
       else{
         setApiResponseForecast(apiResponseForecastJSON);
       }
-
     }
    )();
   }, []);
   
-  if(apiResponseRealtime === null)
-    setApiResponseRealtime(apiResponseRealtimeJSON);
-
-  if(apiResponseForecast === null)
-    setApiResponseForecast(apiResponseForecastJSON);
-
   return (
-    <View style={{flex:1, position:"relative"}}>
-      <Image 
-          source={require("./assets/backgrounds/background2.jpg")}
-          blurRadius={9}
-          style={styles.background}
-        />
-      <StatusBar style='auto' />
-      <SafeAreaView style={{flex:1}}>
-          <PaperProvider theme={theme}>
-            <View style={{flex: 1, justifyContent: 'top', alignItems: 'flex-start', backgroundColor:"transparent"}}>
-              <HeaderSection apiResponseRealtime={apiResponseRealtime} showMenu={showMenu} setMenu={setMenu}/>
-              <WeatherOverviewSection apiResponseRealtime={apiResponseRealtime}/>
-              {/* <DetailsSection apiResponse={apiResponse}/> */}
-              <DailyForecastSection apiResponseRealtime={apiResponseRealtime} apiResponseForecast={apiResponseForecast}/>
-            </View>
-          </PaperProvider>
-      </SafeAreaView>
+    <View style={{flex:1}}>
+      <NavigationContainer theme={theme}>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name="HomeScreen" 
+            options={{
+              headerShown:false
+            }}
+            children={(props) => <HomeScreen {...props} 
+                                  apiResponseRealtime={apiResponseRealtime} 
+                                  apiResponseForecast={apiResponseForecast}
+                                  showMenu={showMenu}
+                                  setMenu={setMenu}
+                                 />}
+          />
+          <Stack.Screen 
+            name="Cities" 
+            options={{
+              headerShown:false
+            }}
+            children={(props) => <CitiesPage {...props}
+                                  citiesList={citiesList}
+                                  setCitiesList={setCitiesList}
+                                 />}
+          />
+          <Stack.Screen name="Settings" component={SettingsPage}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -345,9 +408,9 @@ const styles = StyleSheet.create({
     marginLeft:"7%",
   },
   weatherOverviewSection:{
-    flex:0.75,
+    flex:0.79,
     display:"flex",
-    flexDirection:"row",
+    flexDirection:"column",
     backgroundColor:"transparent",
     width:"100%",
     alignItems: 'center',
@@ -355,8 +418,8 @@ const styles = StyleSheet.create({
   },
   weatherIconSection:{
     backgroundColor:"transparent",
-    width:220,
-    height:220,
+    width:200,
+    height:200,
     alignItems: 'center',
     justifyContent: 'center',
     marginLeft:"0%",
@@ -373,5 +436,5 @@ const styles = StyleSheet.create({
   detailIconStyle:{ 
     width: 38,
     height: 38,
-  }
+  },
 });
